d6768748b2e3a72d4a3ee68ece5fed8eefdc515c
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-05 21:40:06 -0600

    added react-bootstrap, and trying to add a modal for a login for existing users

---
+++ client/src/Components/Modal/LoginModal.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react"
+import {Modal, Button, Row, Col, form} from "react-bootstrap"
+
+class LoginModal extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <Modal
+      {...this.props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Login
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+            <div className="container">
+            Add stuff here. 
+            </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+        )
+    }
+}
+
+export default LoginModal
