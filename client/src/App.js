49933156a85f28139e13a8cafac2996c44af0057
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-07 22:27:41 -0600

    created user page and made a route for it in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import {BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./Pages/homepage"
 import Register from "./Pages/register"
+import User from "./Pages/user"
 function App() {
   return (
       <Router>
            <Route  exact path = "/"  component = {Home}  />
            <Route exact path = "/register" component = {Register} />
+           <Route exact path ="/user" component = {User} />
           
 
       </Router>
