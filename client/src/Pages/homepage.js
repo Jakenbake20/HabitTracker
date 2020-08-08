2fec4c89077d955cf21bff2e3faa4c86f02b2ce4
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-07 22:36:44 -0600

    Changed entire code for Homepage so that it would work with custom nav and login

--- client/src/Pages/homepage.js
+++ client/src/Pages/homepage.js
@@ -1,16 +1,41 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import Navbar from "../Components/Navbar"
-import Register from "./register"
+import { Button, ButtonToolbar } from "react-bootstrap"
+import LoginModal from "../Components/Modal/LoginModal"
+import API from "../utils/api"
 import Login from "../Components/Modal"
-import {Modal, Button, Row, Col, form} from "react-bootstrap"
- 
-class Home extends Component{
 
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { deps: [], addModalShow: false, username: "", fullname: "", email: "", password: "" }
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+        console.log(value)
+    }
+    submit = event => {
+        console.log(this.state.username, this.state.fullname, this.state.email, this.state.password)
+        API.signup({
+            name: this.state.fullname,
+            email: this.state.email,
+            password: this.state.password,
+            username: this.state.username
+        })
+    }
+    handleLogin = event => {
+        API.signin({ username: this.state.username, password: this.state.password }).then(results => {
+            console.log(results)
+        })
+    }
     render() {
+
+        let addModalClose = () => this.setState({ addModalShow: false });
         return (
             <>
-           
-
                 <header id="header">
                 <a href="/" className="logo">Habit Tracker</a>
                     <nav className="left">
@@ -18,15 +43,28 @@ class Home extends Component{
                         <p className="habit">H.T</p>
                         </a>
                     </nav>
+                    <a href="/" className="logo">Habit Tracker</a>
                     
+                    <Login handleInputChange={this.handleInputChange} handleLogin={this.handleLogin} username={this.state.username} password={this.state.password} />
                     <nav className="right">
-                    
-                        {/* <a href="#" className="button alt">Log in</a> */}
+                        <div className="modal">
+                            <nav className="right">
+                            <ButtonToolbar>
+                                <button className="button alt"
+                                    onClick={() => this.setState({ addModalShow: true })}  >Log in</button>
+
+                                <LoginModal
+                                    show={this.state.addModalShow}
+                                    onHide={addModalClose}
+                                />
+                            </ButtonToolbar>
+                            </nav>
+                        </div>
                     </nav>
                 </header>
                 <Navbar />
-            
-             
+
+
 
                 <section id="banner">
                     <div className="content">
@@ -109,6 +147,10 @@ class Home extends Component{
                         </div>
                     </div>
                 </section>
+               
+
+
+
 
 
                 <footer id="footer">
@@ -119,10 +161,10 @@ class Home extends Component{
                         &copy; Copyrighted by Habit Tracker <p>Leo | Jacob | Drew | Adison | Austin</p>
                     </div>
                 </footer>
-
             </>
         )
     }
+
 }
 
 export default Home
