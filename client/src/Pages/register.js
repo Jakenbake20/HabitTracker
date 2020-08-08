fb8009ebe38992398023de3d94c112e8b2b8d871
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-07 21:32:14 -0600

    fixed the look of the loing modal, removed hamburger dropdown menu and created a HT logo.

--- client/src/Pages/register.js
+++ client/src/Pages/register.js
@@ -9,25 +9,25 @@ class Register extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { deps: [], addModalShow: false, username:"", fullname:"", email:"", password:"" }
+        this.state = { deps: [], addModalShow: false, username: "", fullname: "", email: "", password: "" }
     }
 
-    handleInputChange=event=>{
-         const {name, value} = event.target
-         this.setState({[name]:value})
-         console.log(value)
+    handleInputChange = event => {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+        console.log(value)
     }
-    submit=event=>{
+    submit = event => {
         console.log(this.state.username, this.state.fullname, this.state.email, this.state.password)
         API.signup({
-            name:this.state.fullname,
-            email:this.state.email,
-            password:this.state.password,
-            username:this.state.username
+            name: this.state.fullname,
+            email: this.state.email,
+            password: this.state.password,
+            username: this.state.username
         })
     }
-    handleLogin=event=>{
-        API.signin({username:this.state.username, password:this.state.password}).then(results=>{
+    handleLogin = event => {
+        API.signin({ username: this.state.username, password: this.state.password }).then(results => {
             console.log(results)
         })
     }
@@ -37,15 +37,20 @@ class Register extends Component {
         return (
             <>
                 <header id="header">
+                <a href="/" className="logo">Habit Tracker</a>
                     <nav className="left">
-                        <a href="#menu"><span>Menu</span></a>
+                        <a className="habitbox" href="/"> 
+                        <p className="habit">H.T</p>
+                        </a>
                     </nav>
                     <a href="/" className="logo">Habit Tracker</a>
-                <Login handleInputChange={this.handleInputChange} handleLogin={this.handleLogin} username={this.state.username} password={this.state.password} />
+                    
+                    <Login handleInputChange={this.handleInputChange} handleLogin={this.handleLogin} username={this.state.username} password={this.state.password} />
                     <nav className="right">
                         <div className="modal">
+                            <nav className="right">
                             <ButtonToolbar>
-                                <button className="loginBtn"
+                                <button className="button alt"
                                     onClick={() => this.setState({ addModalShow: true })}  >Log in</button>
 
                                 <LoginModal
@@ -53,46 +58,35 @@ class Register extends Component {
                                     onHide={addModalClose}
                                 />
                             </ButtonToolbar>
+                            </nav>
                         </div>
                     </nav>
                 </header>
                 <Navbar />
-              
 
-                <div className="container-fluid" id="register">
 
-                    <div className="input-group mb-3">      
-                        <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1">Choose your Username!</span>
-                        </div>
-                        <input type="text" className="form-control" name="username" value={this.state.username}   onChange={this.handleInputChange} placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
+                <div className="container" id="container" >
+
+                    <div className="input-group mb-3" >
+                        <input type="text" className="form-control" name="username" value={this.state.username} onChange={this.handleInputChange} placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
                     </div>
 
-                    <div className="input-group mb-3">      
-                        <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1">Enter your name</span>
-                        </div>
+                    <div className="input-group mb-3">
                         <input type="text" className="form-control" name="fullname" value={this.state.fullname} onChange={this.handleInputChange} placeholder="Name" aria-label="Name" aria-describedby="basic-addon1"></input>
                     </div>
 
-                    <div className="input-group mb-3">      
-                        <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1">Enter your e-mail</span>
-                        </div>
+                    <div className="input-group mb-3">
                         <input type="text" className="form-control" name="email" placeholder="email" aria-label="email" value={this.state.email} onChange={this.handleInputChange} aria-describedby="basic-addon1"></input>
                     </div>
 
-                    <div className="input-group mb-3">      
-                        <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1">Pick a Password!</span>
-                        </div>
+                    <div className="input-group mb-3">
                         <input type="text" className="form-control" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange} aria-label="password" aria-describedby="basic-addon1"></input>
                     </div>
 
                     <br></br>
-                    <button onClick={this.submit} type="button" className="btn btn-dark">Submit</button>
+                    <button onClick={this.submit} type="button" className="btn btn-dark" id="btmSub">Submit</button>
 
-                
+
                 </div>
 
 
